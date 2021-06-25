aef7904eef95008ec1d2b1e656410df3957fcf78
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-06-25 14:22:56 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by CJ Gaspari on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
